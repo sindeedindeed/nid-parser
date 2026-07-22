a7ae449432009a6e3a5fd1f0606b4bc2c856d832
MAISHAN NADIS <sindeed222@gmail.com>
2026-07-22 10:50:20 +0600

    feat(api): implement /parse-nid endpoint with dual image validation

---
+++ app/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from app.utils import validate_image
+
+app = FastAPI(
+    title = "NID Parser API",
+    description= "AI-powered application to extract and translate NID information.",
+    version="1.0.0"
+)
+
+@app.post("/parse-nid")
+async def parse_nid(
+    front_image: UploadFile = File(..., description="Front side"),
+    back_image: UploadFile = File(..., description="Back side")
+):
+    
+    # using from utils.py file
+    front_bytes = await validate_image(front_image, "front")
+    back_bytes = await validate_image(back_image, "back")
+    
+    return {
+        "status": "Success",
+        "message": "Validation successful. Ready for OCR",
+        "front_image_size": len(front_bytes),
+        "back_image_size": len(back_bytes)
+    }
