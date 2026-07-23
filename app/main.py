401c3e619e8c33823fb0b63eeb639317a842fe76
MAISHAN NADIS <sindeed222@gmail.com>
2026-07-23 10:45:41 +0600

    feat(ui): add static HTML code for visual ui and serve via FileResponse as default GET

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse
 from app.utils import validate_image
+from app.models import NIDResponse
 from app.services import extract_nid_data
+import os
 
 app = FastAPI(
     title = "NID Parser API",
     description= "AI-powered application to extract and translate NID information.",
     version="1.0.0"
 )
+
+@app.get("/", response_class=FileResponse)
+async def serve_ui():
+    file_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "static", "index.html")
+    return FileResponse(file_path)
 
 @app.post("/parse-nid")
 async def parse_nid(
